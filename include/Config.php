270e9f6a7c159c2f5c515830c3c6ae1732bb4267
josephcaxton <Josephcaxtonidowu@yahoo.com>
2014-04-24 10:06:02 +0100

    First commit

---
+++ include/Config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Database configuration
+ * @author Joseph Caxton-Idowu
+ */
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'letmein2');
+define('DB_HOST', '54.72.34.84');
+define('DB_NAME', 'Videos');
+ 
+define('USER_CREATED_SUCCESSFULLY', 0);
+define('USER_CREATE_FAILED', 1);
+define('USER_ALREADY_EXISTED', 2);
+define('USER_ACTIVITY_LOGGED', 3);
+define('USER_ACTIVITY_FAILED', 4);
+?>
